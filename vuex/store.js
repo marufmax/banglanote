9f809e2082738021dc1a16b0f4c603c73b0b191f
Maruf Alom <desk.maruf@gmail.com>
2018-08-12 18:43:25 +0600

    Notes without bangla

--- vuex/store.js
+++ vuex/store.js
@@ -7,3 +7,38 @@ const state = {
   notes: [],
   activeNote: {}
 }
+
+
+const mutations = {
+  //TODO: All posible mutations 
+
+  ADD_NOTE (state) {
+    const newNote = {
+      text: 'New Note',
+      favorite: false
+    }
+
+    // only mutator can mutate the state
+    state.notes.push(newNote);
+    state.activeNote = NewNote;
+  },
+
+  EDIT_NOTE(state, text) {
+    state.activeNote.text = text;
+  },
+  DELETE_NOTE(state, text) {
+    state.notes.$remove(state.activeNote);
+    state.activeNote = state.notes[0];
+  },
+  TOGGLE_FAVORITE(state, note) {
+    state.activeNote.favorite = !state.activeNote.favorite;
+  },  
+  SET_ACTIVE_NOTE(state, note) {
+    state.activeNote = note;
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations
+})
